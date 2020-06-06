7eb25901bb9397ca224022898abb5a16db12174b
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-05 23:29:29 -0300

    05.05 - Melhorando a organização do Layout

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,7 +3,6 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
