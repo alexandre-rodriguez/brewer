3df630daf7ddf41340d2c1feefa602d2e020c8aa
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-07 13:06:22 -0300

    09.02 - Configurando o projeto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
+	
+	@Autowired
+	private Cervejas cervejas;
 
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
+		cervejas.findAll();
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
