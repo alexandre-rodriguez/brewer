0e9080a743203429b77831a37023c4bfca5ab0c3
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-12 21:15:23 -0300

    15.08 - Mostrando total de páginas dinamicamente

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -64,9 +65,8 @@ public class CervejasController {
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());
 		
-		System.out.println(">>> pageNumber: " + pageable.getPageNumber());
-		
-		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable)); 
+		Page<Cerveja> pagina = cervejas.filtrar(cervejaFilter, pageable);
+		mv.addObject("pagina", pagina); 
 		return mv;
 	}
 }
