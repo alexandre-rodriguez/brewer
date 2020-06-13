7598451c9a7c690be7835fa122a0bc671bc548c3
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-13 17:15:19 -0300

    16.09 - Agrupando validações para CPF ou CNPJ

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -5,8 +5,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -30,13 +30,13 @@ public class ClientesController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/clientes/novo", method = RequestMethod.POST)
-	public ModelAndView cadastrar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
+	@PostMapping(value = "/novo")
+	public ModelAndView salvar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(cliente);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
-		return new ModelAndView("redirect:/cliente/novo");
+		return new ModelAndView("redirect:/clientes/novo");
 	}
 }
