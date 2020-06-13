c0e7a7c5ec503b7fee601bde72d03e81773418cf
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-13 15:13:26 -0300

    16.07 - Selecionando cidades pelo estado com Ajax

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -1,24 +1,35 @@
 package com.algaworks.brewer.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
+	
+	@Autowired
+	private Cidades cidades;
 
-	@RequestMapping("/cidades/novo")
+	@RequestMapping("/novo")
 	public String novo(Cidade cidade) {
 		return "cidade/CadastroCidade";
 	}
 	
-	@RequestMapping(value = "/cidades/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(cidade);
@@ -26,5 +37,10 @@ public class CidadesController {
 		
 		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
 		return "redirect:/cidades/novo";
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(@RequestParam(name = "estado", defaultValue="-1") Long codigoEstado) {
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 }
