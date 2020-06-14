3fbf1ee6b67db231455221b7cb1ff99bad72aca5
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-14 12:59:32 -0300

    18.03 - Desafio: Começando o cadastro do usuário

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -4,27 +4,30 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Usuario;
 
 @Controller
+@RequestMapping("/usuarios")
 public class UsuariosController {
 
-	@RequestMapping("/usuarios/novo")
-	public String novo(Usuario usuario) {
-		return "usuario/CadastroUsuario";
+	@RequestMapping("/novo")
+	public ModelAndView novo(Usuario usuario) {
+		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
+		return mv;
 	}
 	
-	@RequestMapping(value = "/usuarios/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+	@PostMapping(value = "/novo")
+	public ModelAndView cadastrar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(usuario);
 		}
 		
-		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
-		return "redirect:/usuarios/novo";
+		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
+		return new ModelAndView("redirect:/usuarios/novo");
 	}
 }
