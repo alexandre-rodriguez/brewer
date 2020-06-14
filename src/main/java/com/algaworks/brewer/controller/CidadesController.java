588e75b667c4050661fde656180500a6c5a52abe
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-13 21:44:11 -0300

    16.14 - Desafio: Finalizar cadastro de cidade

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cidade;
 import com.algaworks.brewer.repository.Cidades;
+import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.CadastroCidadeService;
+import com.algaworks.brewer.service.exception.NomeCidadeJaCadastradaException;
 
 @Controller
 @RequestMapping("/cidades")
@@ -23,20 +27,35 @@ public class CidadesController {
 	
 	@Autowired
 	private Cidades cidades;
+	
+	@Autowired
+	private Estados estados;
+	
+	@Autowired
+	private CadastroCidadeService cadastroCidadeService;
 
-	@RequestMapping("/novo")
-	public String novo(Cidade cidade) {
-		return "cidade/CadastroCidade";
+	@RequestMapping("/nova")
+	public ModelAndView nova(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("cidade/CadastroCidade");
+		mv.addObject("estados", estados.findAll());
+		return mv;
 	}
 	
-	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
+	@PostMapping(value = "/nova")
+	public ModelAndView salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
-			return novo(cidade);
+			return nova(cidade);
+		}
+
+		try {
+			cadastroCidadeService.salvar(cidade);
+		} catch (NomeCidadeJaCadastradaException e) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
+			return nova(cidade);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
-		return "redirect:/cidades/novo";
+		return new ModelAndView("redirect:/cidades/nova");
 	}
 	
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
