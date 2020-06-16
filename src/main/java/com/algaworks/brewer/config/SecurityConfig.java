e3aed44122049325f224c31ed2cb367b9009e573
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-16 16:31:12 -0300

    19.06 - Carregando as permissões do usuário

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/cidades/nova").hasRole("CADASTRAR_CIDADE")
-			.antMatchers("/usuarios/**").hasRole("CADASTRAR_USUARIO")
+			.antMatchers("/cidades/nova").hasAuthority("CADASTRAR_CIDADE")
+			.antMatchers("/usuarios/**").hasAuthority("CADASTRAR_USUARIO")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
