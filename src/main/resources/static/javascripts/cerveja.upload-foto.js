3b726bb93eafc582a4665ffb13acbe5620f7771e
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-16 18:10:45 -0300

    19.09 - Configurando o CSRF

--- src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -19,7 +19,8 @@ Brewer.UploadFoto = (function() {
 				filelimit: 1,
 				allow: '*.(jpg|jpeg|png)',
 				action: this.containerFotoCerveja.data('url-fotos'),
-				complete: onUploadCompleto.bind(this)
+				complete: onUploadCompleto.bind(this),
+				beforeSend: adicionarCsrfToken
 		};
 		
 		UIkit.uploadSelect($('#upload-select'), settings);
@@ -28,6 +29,13 @@ Brewer.UploadFoto = (function() {
 		if(this.inputNomeFoto.val()) {
 			onUploadCompleto.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val() });
 		}
+	}
+	
+	function adicionarCsrfToken(xhr) {
+		var token = $('input[name=_csrf]').val();
+		var header = $('input[name=_csrf_header]').val();
+		
+		xhr.setRequestHeader(header, token);
 	}
 	
 	function onUploadCompleto(resposta) {
