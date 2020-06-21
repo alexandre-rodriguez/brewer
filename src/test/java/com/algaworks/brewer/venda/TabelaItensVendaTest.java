3c675c3217fb6bff53e0fa5f59d1829c26e2d183
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-21 13:20:47 -0300

    23.03 - Criando tabela de itens de venda

---
+++ src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
@@ -0,0 +1,25 @@
+package com.algaworks.brewer.venda;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TabelaItensVendaTest {
+	
+	private TabelaItensVenda tabelaItensVenda;
+	
+	
+	@Before
+	public void setUp() {
+		this.tabelaItensVenda = new TabelaItensVenda();
+	}
+
+	@Test
+	public void deveCalcularValorTotalSemItens() {
+		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+
+}
