6bdac9291e094b6ad04ec87631108206957c5a86
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-24 18:27:22 -0300

    24.02 - Configuração do servidor de e-mail com arquivos externos

---
+++ src/main/java/com/algaworks/brewer/config/MailConfig.java
@@ -0,0 +1,40 @@
+package com.algaworks.brewer.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@PropertySource({ "classpath:env/mail-${ambiente:local}.properties" })
+@PropertySource(value = { "file://${HOME}/.brewer-mail.properties" }, ignoreResourceNotFound = true)
+public class MailConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean JavaMailSender mailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername(env.getProperty("username"));
+		mailSender.setPassword(env.getProperty("password"));
+		
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true); 
+        properties.put("mail.debug", false);
+        properties.put("mail.smtp.connectiontimeout", 5000);  // miliseconds
+
+        mailSender.setJavaMailProperties(properties);
+		
+		return mailSender;
+	}
+}
