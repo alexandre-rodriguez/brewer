087e45774730421f76f32e827713de6d100c8ac3
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-25 19:19:56 -0300

    25.01 - Implementando a exclusão de cerveja

--- src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
+++ src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.service;
 
+import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.Cervejas;
 import com.algaworks.brewer.service.event.cerveja.CervejaSalvaEvent;
+import com.algaworks.brewer.service.exception.ImpossivelExcluirEntidadeException;
+import com.algaworks.brewer.storage.FotoStorage;
 
 @Service
 public class CadastroCervejaService {
@@ -19,10 +22,28 @@ public class CadastroCervejaService {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+	@Autowired
+	FotoStorage fotoStorage;
+	
 	@Transactional
 	public void salvar(Cerveja cerveja) {
 		cervejas.save(cerveja);
 		
 		publisher.publishEvent(new CervejaSalvaEvent(cerveja));
+	}
+
+	@Transactional
+	public void excluir(Cerveja cerveja) {
+		try {
+			String foto = cerveja.getFoto();
+			
+			
+			cervejas.delete(cerveja);
+			cervejas.flush();
+			
+			fotoStorage.excluir(foto);
+		} catch (PersistenceException e) {
+			throw new ImpossivelExcluirEntidadeException("Impossível apagar cerveja. Já foi usada em alguma venda.");
+		}
 	}
 }
