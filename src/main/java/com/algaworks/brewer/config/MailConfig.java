0f4d82170e01fd3c25f81a0b4dca196e70f4593f
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-28 13:30:31 -0300

    28.08 - Fazendo deploy na nuvem

--- src/main/java/com/algaworks/brewer/config/MailConfig.java
+++ src/main/java/com/algaworks/brewer/config/MailConfig.java
@@ -28,7 +28,7 @@ public class MailConfig {
 		mailSender.setHost("smtp.gmail.com");
 		mailSender.setPort(587);
 		mailSender.setUsername(env.getProperty("email.username"));
-		mailSender.setPassword(env.getProperty("email.password"));
+		mailSender.setPassword(env.getProperty("MAIL_PASSWORD"));
 		
         Properties properties = new Properties();
         properties.put("mail.transport.protocol", "smtp");
