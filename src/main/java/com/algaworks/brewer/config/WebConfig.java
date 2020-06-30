7ed2dad62bc2a5904500a7db3c76eaa3b10c02fd
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-06-29 21:05:57 -0300

    31.04 - Usando JCache com EhCache

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,12 +1,15 @@
 package com.algaworks.brewer.config;
 
 import java.math.BigDecimal;
+import java.net.URISyntaxException;
 import java.time.format.DateTimeFormatter;
+
+import javax.cache.Caching;
 
 import org.springframework.beans.BeansException;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
@@ -129,17 +132,10 @@ public class WebConfig  implements ApplicationContextAware, WebMvcConfigurer{
 //	}
 	
 	@Bean
-	public CacheManager cacheManager() {
-//		CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
-//				.maximumSize(3)
-//				.expireAfterAccess(20, TimeUnit.MINUTES);
-//		
-//		GuavaCacheManager cacheManager = new GuavaCacheManager();
-//		cacheManager.setCacheBuilder(cacheBuilder);
-//		
-//		return cacheManager;
-		
-		return new ConcurrentMapCacheManager();
+	public CacheManager cacheManager() throws URISyntaxException {
+		return new JCacheCacheManager(Caching.getCachingProvider().getCacheManager(
+				getClass().getResource("/cache/ehcache.xml").toURI(), 
+				getClass().getClassLoader()));
 	}
 	
 	@Bean
