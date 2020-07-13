0c58721a13c9a279295f9ef0ee104020e80e745e
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-07-13 19:29:54 -0300

    32.20 - Publicando no Heroku

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -75,7 +75,7 @@ public class UsuariosController {
 	@GetMapping
 	public ModelAndView pesquisar(UsuarioFilter usuarioFilter,
 			@PageableDefault(size = 3) Pageable pageable, HttpServletRequest httpServletRequest) {
-		ModelAndView mv = new ModelAndView("/usuario/PesquisaUsuarios");
+		ModelAndView mv = new ModelAndView("usuario/PesquisaUsuarios");
 		mv.addObject("grupos", grupos.findAll());
 		
 		PageWrapper<Usuario> paginaWrapper = 
--- src/main/java/com/algaworks/brewer/controller/VendasController.java
+++ src/main/java/com/algaworks/brewer/controller/VendasController.java
@@ -151,7 +151,7 @@ public class VendasController {
 	@GetMapping
 	public ModelAndView pesquisar(VendaFilter vendaFilter,
 			@PageableDefault(size = 10) Pageable pageable, HttpServletRequest httpServletRequest) {
-		ModelAndView mv = new ModelAndView("/venda/PesquisaVendas");
+		ModelAndView mv = new ModelAndView("venda/PesquisaVendas");
 		mv.addObject("todosStatus", StatusVenda.values());
 		mv.addObject("tiposPessoa", TipoPessoa.values());
 		
